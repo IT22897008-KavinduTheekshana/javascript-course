6b0bdc837fadc1398df5e5c4f4f93a3b3b23930a
KAVINDU <it22897008@my.sliit.lk>
2024-11-30 13:05:36 +0530

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+//Basic Operations
+let js = 'amazing'
+console.log(40 + 8 + 23 - 10)
+
+let firstName = 'John';
+console.log(firstName);
+
+console.log(typeof true);
+console.log(typeof null);
+
+
+const now = 2037
+const ageKavi = now - 2000;
+const ageKasun = now - 1997;
+console.log(ageKavi, ageKasun);
+
+
+//Assignment oprators
+let x = 10+5; //15
+x += 10; //x = x +10 = 25
+x *= 4; //x = x * 4 = 100
+x ++ //x = x+1
+x -- //x = x-1
+
+console.log(x);
+
+
+const massMark = 78
+const heightMark = 1.69
+const massJohn = 92
+const heightJohn = 1.95
+
+const bmiMark = massMark / heightMark ** 2
+const bmiJohn = massJohn / (heightJohn * heightJohn)
+console.log('bmiMark = ' + bmiMark + '\n' + 'bmiJohn =' + bmiJohn)
+
+const markHigherBMI = bmiMark > bmiJohn
+
+console.log(markHigherBMI)
