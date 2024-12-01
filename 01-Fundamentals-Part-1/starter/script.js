4805ee7c78e97cb6e34be1902dddafdd04f5d115
KAVINDU <it22897008@my.sliit.lk>
2024-12-01 08:48:43 +0530

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -96,7 +96,7 @@ if (bmiMark > bmiJohn) {
 } else {
   console.log(`John's BMI(${bmiJohn}) is higher than Mark's (${bmiMark})`)
 }
-*/
+
 
 // Type conversion and Coercion
 
@@ -105,10 +105,28 @@ console.log(Number(inputYear), inputYear)
 console.log(Number(inputYear) + 18)
 
 //console.log(Number('kavindu') - output is NaN
-console.log(String(23),23);
-
+console.log(String(23), 23)
 
 //Type  Coercion
 
-console.log('I am' + 23 + ' years old');
-console.log('23'-'10'-3);
+console.log('I am' + 23 + ' years old')
+console.log('23' - '10' - 3)
+
+*/
+
+//Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Kavindu'));
+console.log(Boolean({}));
+console.log(Boolean(0));
+
+const money = 0;
+if(money){
+    console.log('You have money!');
+}else{
+    console.log('You do not have money!');
+}
