9eccc413e12e0d53693b454f54d582b72b9b2ad6
KAVINDU <it22897008@my.sliit.lk>
2024-12-02 12:28:08 +0530

    function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,8 @@
+//Srict mode
+
 'use strict'
 
+/*
 let hasDriversLicence = false
 const passTest = true
 
@@ -8,3 +11,26 @@ if (hasDriversLicence) console.log('you can drive')
 
 //const interface = 'Audio';
 //const private = 55;
+*/
+
+//function
+
+function logger() {
+  console.log('My name is Kavindu')
+}
+
+logger()
+logger()
+
+function frutProcessor(apples, oranges) {
+  console.log(apples, oranges)
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+  return juice
+}
+
+// const frutJuice  = frutProcessor(5, 2);
+// console.log(frutJuice);
+
+console.log(frutProcessor(5, 2));
+
+
